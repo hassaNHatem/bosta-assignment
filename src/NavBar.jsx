f9d80413703a6fee8b45463bed3e342a53855344
hassaNHatem <hasan.hatem.44@gmail.com>
2024-05-01 01:58:45 +0300

    added cairo font , enabled arabic/english on nav bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import "./styles/navbar.css"
 import { IoMenu } from "react-icons/io5";
+import { MyContext } from "./MyContext";
 function NavBar(){
    const navRef = useRef();
    const [width, setWidth] = useState(window.innerWidth);
    const [displayMobileList , setDisplayMobileList] = useState(false)
+   const {lang , setLang} = useContext(MyContext)
     
-    const showNavBar = ()=>{
-        navRef.current.className.toggle("responsive_nav")
+    const handleLanguagechange = ()=>{
+        if(lang==='en'){
+            setLang('ar')
+        }else{
+            setLang('en')
+        }
+       
     }
-     
+    const handleClasstoggle = ()=>{
+        navRef.current.classToggle("rightToLeft")
+    }
     useEffect(() => {
         const handleResize = () => {
           setWidth(window.innerWidth);
@@ -22,35 +31,43 @@ function NavBar(){
         };
       }, []); 
 
-    return <div className="nav-container">
-                <div className="nav-flex">
+    return <div className={"nav-container"}>
+
+                <div ref={navRef}  className={lang==='en'?"nav-flex":"nav-flex rightToLeft"}>
+
                     <div className="logo">
                         bosta logo here
                     </div>
                     {width>1150&&<div ref={navRef} className="nav-links">
                     <ul className="middle-ul">
-                        <li>main page</li>
-                        <li>Prices</li>
-                        <li>Contact Sales</li>
+                        <li >{lang==='en'?'main page':'الرئيسيه'}</li>
+                        <li>{lang==='en'?'prices':'الاسعار'}</li>
+                        <li>{lang==='en'?'contact sales':'كلم المبيعات'}</li>
                     </ul>
+
                     <ul className="last-ul">
-                        <li>Track your Shipment</li>
-                        <li>Login</li>
-                        <li>ع</li>
+                        <li>{lang==='en'?'Track your shipment':'تتبع شحنتك'}</li>
+                        <li>{lang==='en'?'Login':'تسجيل دخول'}</li>
+                        <li onClick={handleLanguagechange}> {lang==='en'?'ع':'ENG'}</li>
                     </ul>
+
                     </div>}
+
                     {width<=1150&&<div className="nav-btn"  onClick={()=>setDisplayMobileList((data)=>!data)}>
                         <IoMenu/>
                     </div>}
                 </div>
+
                 {displayMobileList&&<div className="mobile-display">
                 <ul className="">
+
                         <li>main page</li>
                         <li>Prices</li>
                         <li>Contact Sales</li>
                         <li>Track your Shipment</li>
                         <li>Login</li>
                         <li>ع</li>
+
                     </ul>
                 </div>}
 
